d46ce149c696e8fa99e4f102986313924ea0affc
Wu Dongxia <361722445@qq.com>
2017-11-03 10:09:25 +0800

    新增视频处理
    - 添加视频处理功能

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 using namespace cv;
 
 void gammaCorrection(Mat& src, Mat& dst, float fGamma);
+Mat dehaze(const Mat& src, int r, double topRatio, double t0, double omega, double eps);
 
 int main(int argc, char *argv[]) {
 
@@ -16,8 +17,10 @@ int main(int argc, char *argv[]) {
         cout << "no input image"<<endl;
         return -1;
     }
+    const string fileName = argv[1];
+    const int type = atoi(argv[2]);
 
-    const string imgName = argv[1];
+//    const string imgName = argv[1];
 //    const string imgName = "images/canon3.bmp";
 
     const int filterRadius = 7;
@@ -34,55 +37,45 @@ int main(int argc, char *argv[]) {
     //regularization parameter
     const double eps = 10E-6;
 
-    Mat src = imread(imgName);
 
-    //输出图片大小
-    cout<<"图片大小为: "<<src.rows <<"*" << src.cols<<endl;
+    if(type == 0){
+        Mat src = imread(fileName);
 
-    //开始计时
-    double start =clock();
+        dehaze(src,filterRadius,topRatio,t0,omega,eps);
 
-    //图片必须为彩色图片
-    CV_Assert(!src.empty() && src.channels() == 3);
+        waitKey(0);
+    } else{
+        VideoCapture videoCapture(fileName);
+        if (!videoCapture.isOpened()){
+            cout<<"视频打开失败"<<endl;
+            return -1;
+        }
+        long totalFrameNumber = videoCapture.get(CAP_PROP_FRAME_COUNT);
+        int frameWidth = videoCapture.get(CAP_PROP_FRAME_WIDTH);
+        int frameHeight = videoCapture.get(CAP_PROP_FRAME_HEIGHT);
+        double rate = videoCapture.get(CAP_PROP_FPS);
 
-    Vec3b atmosphericLight = EstimationAtmosphericLight(src);
-    Vec3f a = atmosphericLight;
-    a[0] = (float)(atmosphericLight[0]/255.0);
-    a[1] = (float)(atmosphericLight[1]/255.0);
-    a[2] = (float)(atmosphericLight[2]/255.0);
-//    Mat transmission = EstimationTransmission(src, atmosphericLight,filterRadius, omega, eps);
-//    transmission.convertTo(transmission,CV_32FC1,1.0/255.0);
-    //图片归一化
-    if (src.type() != CV_32FC3){
-        src.convertTo(src,CV_32FC3,1.0/255.0);
+        long frameStart = 0;
+        videoCapture.set(CAP_PROP_POS_FRAMES,frameStart);
+        long currentFrame = frameStart;
+
+        namedWindow("result");
+
+        while(currentFrame < totalFrameNumber){
+            Mat image;
+            videoCapture.read(image);
+            image = dehaze(image,filterRadius,topRatio,t0,omega,eps);
+            currentFrame++;
+            imshow("result",image);
+            waitKey(1);
+        }
+        videoCapture.release();
+        waitKey(0);
     }
 
-    //计算大气光
-//    Vec3f atmosphericLight = estimateAtmosphericLight(src,filterRadius,topRatio);
-
-    //计算投射图
-    Mat transmission = estimateTransmission(src, a,filterRadius, omega, eps);
-
-    //恢复图像
-    Mat recoverImage = recover(src,transmission,a,t0);
-
-    recoverImage.convertTo(recoverImage,CV_8UC3,255);
-
-    gammaCorrection(recoverImage,recoverImage,0.7);
-
-    //停止计时
-    double stop = clock();
-
-    //输出耗时
-    cout<<"恢复图片耗时: "<<(stop-start)/CLOCKS_PER_SEC*1000 <<"ms"<<endl;
-
-    imshow("source image",src);
-    imshow("transmission map",transmission);
-    imshow("recover image",recoverImage);
-
-    waitKey(0);
     return 0;
 }
+
 
 void gammaCorrection(Mat& src, Mat& dst, float fGamma)
 {
@@ -130,4 +123,54 @@ void gammaCorrection(Mat& src, Mat& dst, float fGamma)
 
         }
     }
+}
+
+Mat dehaze(const Mat& src, int r, double topRatio, double t0, double omega, double eps){
+    //输出图片大小
+    cout<<"图片大小为: "<<src.rows <<"*" << src.cols<<endl;
+
+    //开始计时
+    double start =clock();
+
+    //图片必须为彩色图片
+    CV_Assert(!src.empty() && src.channels() == 3);
+
+    Vec3b atmosphericLight = EstimationAtmosphericLight(src);
+    Vec3f a = atmosphericLight;
+    a[0] = (float)(atmosphericLight[0]/255.0);
+    a[1] = (float)(atmosphericLight[1]/255.0);
+    a[2] = (float)(atmosphericLight[2]/255.0);
+//    Mat transmission = EstimationTransmission(src, atmosphericLight,filterRadius, omega, eps);
+//    transmission.convertTo(transmission,CV_32FC1,1.0/255.0);
+    Mat fSrc;
+    src.convertTo(fSrc,CV_32FC3,1.0/255.0);
+    //图片归一化
+//    if (src.type() != CV_32FC3){
+//        src.convertTo(src,CV_32FC3,1.0/255.0);
+//    }
+
+    //计算大气光
+//    Vec3f atmosphericLight = estimateAtmosphericLight(src,filterRadius,topRatio);
+
+    //计算投射图
+    Mat transmission = estimateTransmission(fSrc, a,r, omega, eps);
+
+    //恢复图像
+    Mat recoverImage = recover(fSrc,transmission,a,t0);
+
+    recoverImage.convertTo(recoverImage,CV_8UC3,255);
+
+    gammaCorrection(recoverImage,recoverImage,0.7);
+
+    //停止计时
+    double stop = clock();
+
+    //输出耗时
+    cout<<"恢复图片耗时: "<<(stop-start)/CLOCKS_PER_SEC*1000 <<"ms"<<endl;
+
+//    imshow("source image",src);
+//    imshow("transmission map",transmission);
+//    imshow("recover image",recoverImage);
+
+    return recoverImage;
 }
